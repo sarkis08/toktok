c398269d87657b6a00389b76f7c94992706f4fa5
mhkormoh <moriehenryk@gmail.com>
2024-04-29 08:39:05 +0000

    Fixing...

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -133,7 +133,7 @@ const AuthForm = () => {
           </div>
         </form>
 
-        <div className="mt-6">
+        {/* <div className="mt-6">
           <div className="relative">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300" />
@@ -156,7 +156,7 @@ const AuthForm = () => {
               onClick={() => socialAction("google")}
             />
           </div>
-        </div>
+        </div> */}
 
         <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
           <div>
